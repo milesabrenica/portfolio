8d09614218a3e63acf5f29836b459b4c53c73734
miles retuta <milesretuta@gmail.com>
2015-11-09 05:39:27 -0800

    first commit

---
+++ js/projects.js
@@ -0,0 +1,130 @@
+var projects = [
+    {
+        id: '1',
+        title: 'Portfolio',
+        description: 'One page layout design using AJAX, JavaScript and JQuery to display projects as well as an overlay with detailed descriptions of selected projects. ',
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'SASS'
+        ],
+        thumbUrl: 'imgs/projects/portfolio/portfolio_hero.png',
+        imgUrl: 'imgs/projects/portfolio/portfolio_hero.png',
+        url: '#'
+    },
+    {    
+        id: '2',
+        title: 'Wedding',
+        description: 'One page site to inform friends and family of upcoming wedding details. This site has a countdown until the day of the wedding. Once the wedding happens, the counter will then count up the days since the couple has been married.', 
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'SASS'
+        ],
+        thumbUrl: 'imgs/projects/wedding/thumb.png',
+        imgUrl: 'imgs/projects/wedding/hero.png',
+        url: 'http://benandmiles.com'
+    },
+    {
+        id: '3',
+        title: 'Form',
+        description: 'A form to submit contact information desired quote type. Customer is then sent an email with all form submitted answers as confirmation.',
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        thumbUrl: 'imgs/projects/form/thumb.png',
+        imgUrl: 'imgs/projects/form/hero.png',
+        url: '#'
+    },
+    {
+        id: '4',
+        title: 'Fashion Blog',
+        description: 'Blog to demonstrate layout structures.',
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        thumbUrl: 'imgs/projects/sartres-list/thumb.png',
+        imgUrl: 'imgs/projects/sartres-list/hero.png',
+        url: '#'
+    },
+     {
+        id: '5',
+        title: 'Ecardly',
+        description: 'Demonstrating layout structure in blog format through Wordpress. Manipulated Wordpress templates to repeat blog entry structure with new content.',
+        technologies: [
+            'HTML',
+            'CSS',
+        ],
+        thumbUrl: 'imgs/projects/ecardly/thumb.png',
+        imgUrl: 'imgs/projects/ecardly/hero.png',
+        url: '#'
+    },
+    {
+        id: '6',
+        title: 'matchmaker',
+        description: 'Incoperating JSON and AJAX to display contact information based on what type of professional user is searching for.',
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        thumbUrl: 'imgs/projects/startupmatchmaker/thumb.png',
+        imgUrl: 'imgs/projects/startupmatchmaker/hero.png',
+        url: '#'
+    },
+    {
+        id: '7',
+        title: 'Rock Paper Scissors',
+        description: 'Demonstrating simple form validation by setting values to each option and alerting who wins that round.',
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        thumbUrl: 'imgs/projects/rockpaperscissors/thumb.png',
+        imgUrl: 'imgs/projects/rockpaperscissors/hero.png',
+        url: '#'
+    },
+    {
+        id: '8',
+        title: 'Whole Body Salon',
+        description: 'Demonstrating layout and links to multiple pages that user selects.',
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        thumbUrl: 'imgs/projects/bodysalon/wholebodysalon_thumb.png',
+        imgUrl: 'imgs/projects/bodysalon/wholebodysalon_hero.png',
+        url: '#'
+    },
+    {
+        id: '9',
+        title: 'Boyhood',
+        description: 'Demonstrating layout and hover effects to attain movie details.',
+        technologies: [
+            'AJAX',
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        thumbUrl: 'imgs/projects/boyhood/boyhood_thumb.png',
+        imgUrl: 'imgs/projects/boyhood/boyhood_hero.png',
+        url: '#'
+    }
+
+]
+
---
+++ js/site.js
@@ -0,0 +1,96 @@
+jQuery(document).ready(function($) {
+	
+//NAV LINKS
+  $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+        }
+      }
+    });
+  });
+
+
+$(".project").click(function(){
+  $(".project-overlay").fadeIn(400);
+  $(".main-nav, .main-nav-scrolled").fadeOut(400);
+  $("body").addClass('no_scroll');
+});
+
+
+$(".close").click(function(){
+  $("body").removeClass('no_scroll');
+  $(".project-overlay").fadeOut(400);
+  $(".main-nav, .main-nav-scrolled").fadeIn();
+  
+});
+
+$(".project-overlay").click(function(){
+  $("body").removeClass('no_scroll');
+  $(".project-overlay").fadeOut(400);
+  $(".main-nav, .main-nav-scrolled").fadeIn();
+  
+});
+
+  // NAVIGATION SCROLL
+  var  mn = $(".main-nav"),
+  mns = "main-nav-scrolled",
+  hdr = $('#landing').height();
+
+  $(window).scroll(function() {
+  
+    if( $(this).scrollTop() > hdr ) {
+      mn.addClass(mns);
+    } 
+    else {
+    mn.removeClass(mns);
+    }
+  });
+});
+
+// PROJECT GRID
+
+  var project = $('.project:first').remove();
+
+  for(var i = 0; i < projects.length; i++){
+    var tempProject = project.clone();
+    tempProject.attr('data-id', projects[i].id);
+    tempProject.find('img').attr('src', projects[i].thumbUrl);
+    $('#recent-work').append(tempProject);
+  }
+
+
+
+//PROJECT OVERLAY
+  $('.project').click(function(){
+
+    var myid = $(this).attr('data-id');
+
+    for(var i = 0; i < projects.length; i++){
+      if (projects[i].id == myid){
+        $('.project-details-wrap').find('ul').html("");        
+
+        $('.project-details-wrap').find('img').attr('src', projects[i].imgUrl);
+        $('.project-details-wrap').find('h2').html(projects[i].title);  
+        $('.project-details-wrap').find('p').html(projects[i].description);  
+        $('.project-details-wrap').find('a').attr('href',projects[i].url);  
+        
+        //This goes through the list of technologies and makes a list of them on the window. 
+        for (var k = 0; k < projects[i].technologies.length; k++) {
+          //with javascript create a NEW li element with the contents of "projects[i].technologies[k]"
+          //append that li element to ul
+          
+          $('.project-details-wrap').find('ul').append("<li>"+projects[i].technologies[k]+"</li>");
+        }
+      }
+    }
+  });
+
+
+
